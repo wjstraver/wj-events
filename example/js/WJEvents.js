76714eed0b95cceecdef4f769d34f98748e7d41a
Willem-jan Straver <willem@magicbullet.nl>
2017-01-13 20:25:42 +0100

    big change on trigger function -> array functions now gets cloned before triggered to avoid weird behaviour after function adds itself or other listeners

--- example/js/WJEvents.js
+++ example/js/WJEvents.js
@@ -17,7 +17,7 @@
 		 * @return null
 		 */
 		on: function(eventName, callback){
-			_add.call(this, 'on', eventName, callback);
+			_add(this, 'on', eventName, callback);
 		},
 		/**
 		 * Add function to event once list
@@ -28,7 +28,7 @@
 		 * @return null
 		 */
 		once: function(eventName, callback){
-			_add.call(this, 'once', eventName, callback);
+			_add(this, 'once', eventName, callback);
 		},
 		/**
 		 * Trigger the event with optional arguments
@@ -39,8 +39,16 @@
 		 * @return null
 		 */
 		trigger: function(eventName){
-			_trigger.apply(this, arguments);
-			
+			if(_legal("string",eventName) && this._events) {
+				var args = Array.apply(null, arguments);
+				args.shift();
+
+				var fns = ( this._events.on && this._events.on[eventName] )? this._events.on[eventName].concat() : [];
+				if( this._events.once && this._events.once[eventName] ){
+					fns = fns.concat(this._events.once[eventName]);
+				}
+				_trigger(fns, args);
+			}
 		},
 		/**
 		 * Removes function from the event list and once list
@@ -51,7 +59,7 @@
 		 * @return null
 		 */
 		off: function(eventName, callback){
-			_off.call(this, eventName, callback);
+			_off(this, eventName, callback);
 		},
 		/**
 		 * Removes all callback functions and events from the main object
@@ -65,45 +73,34 @@
 	};
 
 	//_____Private__________________________________________________________________________________/
-	var _add = function(where, eventName, callback){
+	var _add = function(ctx, where, eventName, callback){
 		if(_legal("string",eventName) && _legal("function",callback)) {
-			if (!this._events) this._events = {};
-			if (!this._events[where]) this._events[where] = {};
-			if (!this._events[where][eventName]) this._events[where][eventName] = [];
+			if (!ctx._events) ctx._events = {};
+			if (!ctx._events[where]) ctx._events[where] = {};
+			if (!ctx._events[where][eventName]) ctx._events[where][eventName] = [];
 
-			if (_exist(this._events[where], eventName, callback) === false) this._events[where][eventName].push(callback);			
+			if (_exist(ctx._events[where], eventName, callback) === false) ctx._events[where][eventName].push(callback);			
 		}
 	};
 
-	var _trigger = function(eventName){
-		var args = Array.apply(null,arguments);
-		args.shift();
-		if(_legal("string",eventName) && this._events) {
-			if (this._events.on && this._events.on[eventName]){
-				for (var i=0; i < this._events.on[eventName].length; i++) this._events.on[eventName][i].apply(this._events.on[eventName][i], args);
-			}
-			if (this._events.once && this._events.once[eventName]) while (this._events.once[eventName][0]){ 
-				var f = this._events.once[eventName].shift();
-				f.apply(f,args);
-			}
-		}
-	};
+	var _trigger = function(fns, args){
+		for(var i=0; i<fns.length; i++) fns[i].apply(fns[i], args);
+	}
 
-	var _off = function(eventName, callback){
-		if(_legal("string",eventName) && this._events){
+	var _off = function(ctx, eventName, callback){
+		if(_legal("string",eventName) && ctx._events){
 		    if(_legal("function",callback)){
-		        if(this._events.on){
-			        var i = _exist(this._events.on, eventName, callback);
-			        if (i !== false) this._events.on[eventName].splice(i, 1);
+		        if(ctx._events.on){
+			        var i = _exist(ctx._events.on, eventName, callback);
+			        if (i !== false) ctx._events.on[eventName].splice(i, 1);
 			    }
-			    if(this._events.once){
-			        i = _exist(this._events.once, eventName, callback);
-			        if (i !== false) this._events.once[eventName].splice(i, 1);
+			    if(ctx._events.once){
+			        i = _exist(ctx._events.once, eventName, callback);
+			        if (i !== false) ctx._events.once[eventName].splice(i, 1);
 			    }
 		    } else if(callback === undefined){
-
-		        if (this._events.on) delete this._events.on[eventName];
-		        if (this._events.once) delete this._events.once[eventName];
+		        if (ctx._events.on) delete ctx._events.on[eventName];
+		        if (ctx._events.once) delete ctx._events.once[eventName];
 		    }
 		}
 	};
